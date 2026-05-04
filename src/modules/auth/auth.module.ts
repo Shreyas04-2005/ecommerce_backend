ab692b601b53d2a41eed5f3d10b389320cba6de8
Shreyas04-2005 <patilshreyas547@gmail.com>
2026-05-04 11:07:51 +0530

    changes jwt expiry to 1h

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -10,7 +10,7 @@ import { JwtModule } from "@nestjs/jwt";
     imports: [
     JwtModule.register({
       secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '1d' },
+      signOptions: { expiresIn: '1h' },
     }),
   ],
   controllers: [AuthController],
